8237482d8585a2cc015d5e382fbfd53a34123f7d
waylaa <66755323+waylaa@users.noreply.github.com>
2026-09-11 22:22:30 +0300

    remove GetData method

--- Suruga/Audio/Primitives/CommandResult.cs
+++ Suruga/Audio/Primitives/CommandResult.cs
@@ -1,7 +1,3 @@
 ﻿namespace Suruga.Audio.Primitives;
 
-internal readonly record struct CommandResult(CommandStatus Status, object? Data = null)
-{
-    internal T? GetData<T>() where T : class
-        => Data is T value ? value : null;
-}
+internal readonly record struct CommandResult(CommandStatus Status, object? Data = null);
