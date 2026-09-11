819f979457dfab714e64bf0add855fc2918d92f6
waylaa <66755323+waylaa@users.noreply.github.com>
2026-09-11 22:24:09 +0300

    remove System.Text.Json usage

--- Suruga/Services/CommandRegistrationService.cs
+++ Suruga/Services/CommandRegistrationService.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NetCord;
