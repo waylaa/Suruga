32229d4d3f6af7ad8cd53dd02e6c8674b8c296ac
waylaa <66755323+waylaa@users.noreply.github.com>
2026-09-13 13:22:01 +0300

    primary constructors

--- Suruga/Handlers/VoiceChannelOccupancyReader.cs
+++ Suruga/Handlers/VoiceChannelOccupancyReader.cs
@@ -2,13 +2,8 @@ using NetCord.Gateway;
 
 namespace Suruga.Handlers;
 
-internal sealed class VoiceChannelOccupancyReader
+internal sealed class VoiceChannelOccupancyReader(GatewayClient client)
 {
-    private readonly GatewayClient _client;
-    
-    internal VoiceChannelOccupancyReader(GatewayClient client)
-        => _client = client;
-    
     internal int? GetOtherUsersCount(ulong guildId)
     {
         ulong? channelId = GetCurrentVoiceChannelId(guildId);
@@ -17,22 +12,22 @@ internal sealed class VoiceChannelOccupancyReader
 
     internal ulong? GetCurrentVoiceChannelId(ulong guildId)
     {
-        if (!_client.Cache.Guilds.TryGetValue(guildId, out Guild? guild))
+        if (!client.Cache.Guilds.TryGetValue(guildId, out Guild? guild))
         {
             return null;
         }
 
-        return guild.VoiceStates.TryGetValue(_client.Id, out VoiceState? selfState) ? selfState.ChannelId : null;
+        return guild.VoiceStates.TryGetValue(client.Id, out VoiceState? selfState) ? selfState.ChannelId : null;
     }
 
     private int? CountOthers(ulong guildId, ulong channelId)
     {
-        if (!_client.Cache.Guilds.TryGetValue(guildId, out Guild? guild))
+        if (!client.Cache.Guilds.TryGetValue(guildId, out Guild? guild))
         {
             return null;
         }
 
-        ulong selfId = _client.Id;
+        ulong selfId = client.Id;
         return guild.VoiceStates.Values.Count(s => s.ChannelId == channelId && s.UserId != selfId);
     }
 }
