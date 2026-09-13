8bd99f7d6e81f70f8eafb3174c8489a2bd0d63c4
waylaa <66755323+waylaa@users.noreply.github.com>
2026-09-13 13:22:24 +0300

    remove logger ctor dependency + formatting

--- Suruga/Transport/ReadOnlyAudioByteStreamFactory.cs
+++ Suruga/Transport/ReadOnlyAudioByteStreamFactory.cs
@@ -1,39 +1,20 @@
-﻿using Microsoft.Extensions.Logging;
-using Suruga.Resolvers.Primitives;
+﻿using Suruga.Resolvers.Primitives;
 using Suruga.Resolvers.Sources;
+using Suruga.Transport.Local;
+using Suruga.Transport.Youtube;
 
 namespace Suruga.Transport;
 
-/// <summary>
-/// Factory for creating platform‑specific <see cref="ReadOnlyAudioByteStream"/> instances.
-/// </summary>
-/// <param name="clientFactory">The HTTP client factory used to obtain named clients.</param>
-/// <param name="loggerFactory">The logging factory used to create stream‑specific loggers.</param>
-internal sealed class ReadOnlyAudioByteStreamFactory(IHttpClientFactory clientFactory, ILoggerFactory loggerFactory)
+internal sealed class ReadOnlyAudioByteStreamFactory(IHttpClientFactory clientFactory)
 {
-    /// <summary>
-    /// Creates a <see cref="ReadOnlyAudioByteStream"/> from the specified audio source.
-    /// </summary>
-    /// <param name="source">The audio source.</param>
-    /// <returns>A configured audio byte stream ready for reading.</returns>
-    /// <exception cref="InvalidOperationException">
-    /// Thrown when <paramref name="source"/> is of an unsupported type.
-    /// </exception>
-    internal ReadOnlyAudioByteStream Create(StreamSource source)
+    internal ReadOnlyAudioByteStream Create(StreamSource source) => source switch
     {
-        return source switch
-        {
-            LocalStreamSource { FilePath: string filePath }
-                => new Local.LocalReadOnlyAudioByteStream(filePath, loggerFactory.CreateLogger<Local.LocalReadOnlyAudioByteStream>()),
-            
-            YoutubeStreamSource { Formats: IReadOnlyList<AdaptiveFormat> formats } => new Youtube.YoutubeReadOnlyAudioByteStream
-            (
-                clientFactory.CreateClient("youtube-bytestream"),
-                formats,
-                loggerFactory.CreateLogger<Youtube.YoutubeReadOnlyAudioByteStream>()
-            ),
-            
-            _ => throw new InvalidOperationException("Unsupported platform.")
-        };
-    }
+        LocalStreamSource { FilePath: string filePath } => new LocalReadOnlyAudioByteStream(filePath),
+        YoutubeStreamSource { Formats: IReadOnlyList<AdaptiveFormat> formats } => new YoutubeReadOnlyAudioByteStream
+        (
+            clientFactory.CreateClient("youtube-bytestream"),
+            formats
+        ),
+        _ => throw new InvalidOperationException("Unsupported platform.")
+    };
 }
